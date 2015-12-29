9161f37ff58ea2fe140e56d2410e55605ad54338
kevin <k.hirczy@gmx.at>
2015-12-29 23:08:09 +0100

    i should commit more often

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+  // Ghosthunter configuration
   $('#search-field').ghostHunter({
     results: '#search-results',
     before: function() {
@@ -16,9 +17,16 @@ $(function() {
     $('#search-field').focus();
   });
 
-  var href = window.location.href.replace(/\/$/, "");
-  if(href.indexOf('/tag/') > -1) {
-    var tag  = href.substr(href.lastIndexOf('/') + 1);
+
+  // Helper function to get url segments
+  function getSegment(segment) {
+    var segments = (window.location.pathname).split('/').filter(Boolean);
+    return segments[segment];
+  }
+
+  // Highlight current tags
+  if(getSegment(0) === 'tag') {
+    var tag = getSegment(1);
     $('nav.tags a').each(function() {
       var slug = $(this).data('slug');
       if(typeof tag != "undefined") {
@@ -30,4 +38,42 @@ $(function() {
   } else {
     $('nav.tags a[data-slug="all"]').addClass('selected');
   }
+
+  // Same for current author
+  if(getSegment(0) === 'author') {
+    var author = getSegment(1);
+    $('nav.authors a').each(function() {
+      var slug = $(this).data('slug');
+      if(typeof author != "undefined") {
+        if(slug == author) {
+          $(this).addClass('selected');
+        }
+      }
+    });
+  }
+
+  // Highlight current link in navigation
+  // Since it seems like the `{{current}}` is bugged.
+  // Or I'm just too stupid for it.
+  if(typeof getSegment(0) != 'undefined' && getSegment(0) !== 'page') {
+    $('a[data-nav-slug=' + getSegment(0) + ']').addClass('current-item');
+  } else {
+    $('a[data-nav-slug!=""][data-nav-slug]').first().addClass('current-item');
+  }
+
+
+  // Smooth scrolling
+  // by http://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $('a[data-scroll="true"]').click(function() {
+    if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if(target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 70
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
