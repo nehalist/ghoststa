af17f6d12c325a1773bc9d49960a107c044e6989
nehalist <github@nehalist.io>
2018-06-23 17:59:32 +0200

    removed grunt and bower in favor of webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const devMode = process.env.NODE_ENV !== 'production';
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: ['./src/scripts/index.ts', './src/stylesheets/main.scss'],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: devMode
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: devMode
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss', '.css']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'assets/dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorOptions: { safe: true, discardComments: { removeAll: true }}
+    })
+  ],
+  mode: process.env.NODE_ENV || 'production'
+};
