ffba222f40e67e04aefd04a111fa8d6d7903d069
nehalist <github@nehalist.io>
2018-06-27 06:14:19 +0200

    removed typescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
   entry: [
-    './src/scripts/index.ts',
+    './src/scripts/index.js',
     './src/stylesheets/main.scss',
 
     './src/stylesheets/bootstrap.scss',
@@ -17,11 +17,6 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
